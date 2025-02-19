c1b91780a437c3771a731e4e6d8994df2a096665
HugoK1 <hugo.kaidl@sunnycanadian.cz>
2025-02-19 11:21:07 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
 tiles.setCurrentTilemap(tilemap`level2`)
+let mySprite = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f 2 2 2 2 2 2 f f f . . 
+    . . f f 2 2 2 2 2 2 2 2 2 f . . 
+    . . f 2 2 f f f f f f 2 2 f . . 
+    . . f f f 2 2 2 2 2 f f 2 f . . 
+    . f f 2 2 2 f 2 2 f 2 2 2 f f . 
+    . f 2 2 2 2 f 2 f f 2 2 2 2 f . 
+    . . 2 2 2 f f 2 f 2 2 2 2 f . . 
+    . . . 2 2 2 2 2 2 2 2 2 f . . . 
+    . . 2 2 f f 2 2 2 f 2 f 2 2 . . 
+    . . 2 2 2 f f 2 f f 2 f 2 2 . . 
+    . . 2 2 f 2 f f f 2 2 f 2 2 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
